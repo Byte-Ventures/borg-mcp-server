14e7df0c53b32d1824efde328e2af2c58f31fa27
Theodor Storm <theodor@byteventures.se>
2026-08-07 15:57:33 +0200

    release: prepare 0.14.2

--- test/threat-model.test.ts
+++ test/threat-model.test.ts
@@ -40,7 +40,7 @@ describe("server threat-model document and release metadata", () => {
     );
     expect(manifest).toMatchObject({
       private: false,
-      version: "0.14.1",
+      version: "0.14.2",
       license: "SEE LICENSE IN LICENSE",
     });
   });

3339e347045b583118e7c6845e674d7d3853fdae
Theodor Storm <theodor@byteventures.se>
2026-08-18 12:01:14 +0200

    release: prepare borgmcp-server 3.0.1

--- test/threat-model.test.ts
+++ test/threat-model.test.ts
@@ -40,7 +40,7 @@ describe("server threat-model document and release metadata", () => {
     );
     expect(manifest).toMatchObject({
       private: false,
-      version: "3.0.0",
+      version: "3.0.1",
       license: "SEE LICENSE IN LICENSE",
     });
   });
